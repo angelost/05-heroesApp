a683d01027f6b3c27adc04b1bdb7b65b9839dfa0
Angelo <angelo.gonzalez@live.com>
2022-08-24 14:58:43 -0400

    196. Diseño de la pantalla de Héroe

--- src/app/heroes/pages/heroe/heroe.component.ts
+++ src/app/heroes/pages/heroe/heroe.component.ts
@@ -1,5 +1,5 @@
 import { Component, OnInit } from '@angular/core';
-import { ActivatedRoute } from '@angular/router';
+import { ActivatedRoute, Router } from '@angular/router';
 import { Heroe } from '../../interfaces/heroes.interface';
 import { HeroesService } from '../../services/heroes.service';
 import { switchMap } from "rxjs/operators";
@@ -7,15 +7,20 @@ import { switchMap } from "rxjs/operators";
 @Component({
   selector: 'app-heroe',
   templateUrl: './heroe.component.html',
-  styles: [
-  ]
+  styles: [`
+    img {
+      width: 100%;
+      border-radius: 5px;
+    }
+  `]
 })
 export class HeroeComponent implements OnInit {
 
   heroe!: Heroe;
 
   constructor( private activatedRoute: ActivatedRoute,
-               private heroesService: HeroesService ) { }
+               private heroesService: HeroesService,
+               private router: Router ) { }
 
   ngOnInit(): void {
 
@@ -25,6 +30,10 @@ export class HeroeComponent implements OnInit {
       )
       .subscribe( heroe => this.heroe = heroe );    
 
+  }
+
+  regresar() {
+    this.router.navigate(['/heroes/listado']);
   }
 
 }
