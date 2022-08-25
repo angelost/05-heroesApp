c8d65abc0b98628858d96aef9f76eafa8f2e58ca
Angelo <angelo.gonzalez@live.com>
2022-08-25 19:28:12 -0400

    210. Excepciones en nuestro ImagenPipe

--- src/app/heroes/pages/agregar/agregar.component.ts
+++ src/app/heroes/pages/agregar/agregar.component.ts
@@ -7,7 +7,12 @@ import {  switchMap } from "rxjs/operators";
 @Component({
   selector: 'app-agregar',
   templateUrl: './agregar.component.html',
-  styles: [
+  styles: [ `
+    img {
+      width: 100%;
+      border-radius: 5px
+    }
+  `
   ]
 })
 export class AgregarComponent implements OnInit {
@@ -37,6 +42,11 @@ heroe: Heroe = {
                private router: Router) { }
 
   ngOnInit(): void {
+
+    if( !this.router.url.includes('editar')) {
+      return;
+    }
+
     this.activatedRoute.params
       .pipe(
         switchMap( ({ id }) => this.heroesService.getHeroePorId( id ))
